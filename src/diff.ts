3bb2d349ba5d10ff1609acdeda3ab5e63ee69a1e
Emilio Platzer <emilioplatzer@gmail.com>
2026-04-04 08:33:15 -0300

    Ignora white-spaces

--- src/diff.ts
+++ src/diff.ts
@@ -4,6 +4,6 @@ import { createTwoFilesPatch } from 'diff';
 export function generateDiff(sourceFile: string, targetFile: string, outFile: string): void {
   const source = fs.readFileSync(sourceFile, 'utf-8');
   const target = fs.readFileSync(targetFile, 'utf-8');
-  const patch  = createTwoFilesPatch(sourceFile, targetFile, source, target);
+  const patch  = createTwoFilesPatch(sourceFile, targetFile, source, target, '', '', { ignoreWhitespace: true });
   fs.writeFileSync(outFile, patch);
 }
